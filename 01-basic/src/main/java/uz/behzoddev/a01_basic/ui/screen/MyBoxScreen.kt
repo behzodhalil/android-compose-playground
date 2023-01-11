e68a9d035e33d875faa615c9960777b9a7b92a29
Behzod Halil <behzoddev@gmail.com>
2023-01-11 13:44:07 +0900

    Add MyBoxScreen.kt

---
+++ 01-basic/src/main/java/uz/behzoddev/a01_basic/ui/screen/MyBoxScreen.kt
@@ -0,0 +1,24 @@
+package uz.behzoddev.a01_basic.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import uz.behzoddev.a01_basic.ui.theme.AndroidComposePlaygroundTheme
+
+@Composable
+fun MyBoxScreen() {
+}
+
+@Composable
+fun MyBox(
+    modifier: Modifier = Modifier,
+    contentModifier: Modifier = Modifier
+) {
+
+}
+
+@Composable
+fun MyBoxPreview() {
+    AndroidComposePlaygroundTheme {
+
+    }
+}
