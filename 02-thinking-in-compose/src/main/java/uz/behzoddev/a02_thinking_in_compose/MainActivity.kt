7647e5731e98e9e37d92da66fd0a2f21157826cb
Behzod Halil <behzoddev@gmail.com>
2023-01-13 11:21:32 +0900

    Update MainActivity

--- 02-thinking-in-compose/src/main/java/uz/behzoddev/a02_thinking_in_compose/MainActivity.kt
+++ 02-thinking-in-compose/src/main/java/uz/behzoddev/a02_thinking_in_compose/MainActivity.kt
@@ -3,13 +3,10 @@ package uz.behzoddev.a02_thinking_in_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import uz.behzoddev.a02_thinking_in_compose.`01-basic`.GreetingElevatedButtonExpanded
 import uz.behzoddev.a02_thinking_in_compose.ui.theme.AndroidcomposeplaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidcomposeplaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+               GreetingElevatedButtonExpanded()
             }
         }
     }
