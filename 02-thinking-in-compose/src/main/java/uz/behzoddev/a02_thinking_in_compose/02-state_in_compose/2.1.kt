9b908a8e4468bb27932cc9b30117ba2b3695b6c8
behzoddev <behzoddev@gmail.com>
2023-01-16 21:57:37 +0900

    Add 2.1

--- 02-thinking-in-compose/src/main/java/uz/behzoddev/a02_thinking_in_compose/02-state_in_compose/2.1.kt
+++ 02-thinking-in-compose/src/main/java/uz/behzoddev/a02_thinking_in_compose/02-state_in_compose/2.1.kt
@@ -2,10 +2,14 @@ package uz.behzoddev.a02_thinking_in_compose.`02-state_in_compose`
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,19 +21,58 @@ fun WaterScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+
     Surface(modifier = modifier.padding(8.dp)) {
         Row() {
-            Text(
-                text = "You've received $count",
-                modifier = modifier.padding(8.dp)
-            )
+            val count: MutableState<Int> = remember {
+                mutableStateOf(0)
+            }
+            if (count.value > 0) {
+                val showTask = remember {
+                    mutableStateOf(true)
+                }
+
+                if (showTask.value) {
+                    WaterTask(
+                        taskName = "Have you taken your 15 minute walk today?",
+                        onClick = { showTask.value = false })
+                }
+                Text(
+                    text = "You've received ${count.value}",
+                    modifier = modifier.padding(8.dp)
+                )
+            }
+
             Button(
-                onClick = { count++ },
+                onClick = { count.value++ },
+                enabled = count.value<10,
                 modifier = modifier.padding(start = 12.dp)
             ) {
                 Text(text = "Add")
             }
+        }
+    }
+}
+
+@Composable
+fun WaterTask(
+    taskName: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = modifier
+                .weight(1F)
+                .padding(16.dp),
+            text = taskName
+        )
+
+        IconButton(onClick = { onClick() }) {
+            Icon(imageVector = Icons.Filled.Close, contentDescription = "")
         }
     }
 }
