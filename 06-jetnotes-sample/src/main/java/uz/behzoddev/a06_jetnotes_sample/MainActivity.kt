0a80f6c38df37d042e32b33dcac5dbbe8a4e1f92
Behzod Halil <behzoddev@gmail.com>
2023-01-18 12:34:06 +0900

    Update MainActivity

--- 06-jetnotes-sample/src/main/java/uz/behzoddev/a06_jetnotes_sample/MainActivity.kt
+++ 06-jetnotes-sample/src/main/java/uz/behzoddev/a06_jetnotes_sample/MainActivity.kt
@@ -3,21 +3,17 @@ package uz.behzoddev.a06_jetnotes_sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import uz.behzoddev.a06_jetnotes_sample.`core-ui`.theme.AndroidComposePlaygroundTheme
-import uz.behzoddev.a06_jetnotes_sample.screen.JetNoteUi
+import uz.behzoddev.a06_jetnotes_sample.core_ui.theme.AndroidComposePlaygroundTheme
+import uz.behzoddev.a06_jetnotes_sample.presentation.screen.NoteScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetNoteUi()
+            NoteScreen()
         }
     }
 }
