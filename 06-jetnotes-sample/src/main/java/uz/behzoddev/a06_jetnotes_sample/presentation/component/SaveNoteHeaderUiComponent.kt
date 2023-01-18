83750f01e1fa273f2c2cf95f5bc938ab60584369
Behzod Halil <behzoddev@gmail.com>
2023-01-18 17:39:51 +0900

    Update SaveNoteHeaderUiComponent.kt

--- 06-jetnotes-sample/src/main/java/uz/behzoddev/a06_jetnotes_sample/presentation/component/SaveNoteHeaderUiComponent.kt
+++ 06-jetnotes-sample/src/main/java/uz/behzoddev/a06_jetnotes_sample/presentation/component/SaveNoteHeaderUiComponent.kt
@@ -1,5 +1,11 @@
 package uz.behzoddev.a06_jetnotes_sample.presentation.component
 
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -11,11 +17,93 @@ fun SaveOrUpdateNoteHeaderComponent(
     onOpenColorPickerClick: () -> Unit,
     onDeleteNoteClick: () -> Unit
 ) {
+    TopAppBar(
+        title = {
+            SaveNoteHeaderTextUiComponent()
+        },
+        navigationIcon = {
+            SaveNoteHeaderIconUiComponent(onBackClick)
+        },
+        actions = {
+            SaveNoteHeaderCheckIconUiComponent(onSaveNoteClick)
+            SaveNoteHeaderPickerIconUiComponent(onOpenColorPickerClick)
+            SaveNoteHeaderDeleteIconUiComponent(
+                isUpdateMode = isUpdateMode,
+                onClick = onDeleteNoteClick
+            )
+        }
+    )
+}
 
+@Composable
+fun SaveNoteHeaderTextUiComponent() {
+    Text(
+        text = "Save Note",
+        color = MaterialTheme.colors.onPrimary
+    )
+}
+
+@Composable
+fun SaveNoteHeaderIconUiComponent(onClick: () -> Unit) {
+    IconButton(onClick = { onClick }) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "Save Note Button",
+            tint = MaterialTheme.colors.onPrimary
+        )
+    }
+}
+
+@Composable
+fun SaveNoteHeaderCheckIconUiComponent(
+    onClick: () -> Unit
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Default.Check,
+            tint = MaterialTheme.colors.onPrimary,
+            contentDescription = "Save note"
+        )
+    }
+}
+
+@Composable
+fun SaveNoteHeaderPickerIconUiComponent(
+    onClick: () -> Unit
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = Icons.Filled.PlayArrow,
+            tint = MaterialTheme.colors.onPrimary,
+            contentDescription = "Save note"
+        )
+    }
+}
+
+@Composable
+fun SaveNoteHeaderDeleteIconUiComponent(
+    onClick: () -> Unit,
+    isUpdateMode: Boolean
+) {
+    if (isUpdateMode) {
+        IconButton(onClick = onClick) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete Note Button",
+                tint = MaterialTheme.colors.onPrimary
+            )
+        }
+    }
 }
 
 @Composable
 @Preview(showBackground = true)
 fun SaveOrUpdateNoteHeaderComponentPreview() {
-
+    SaveOrUpdateNoteHeaderComponent(
+        isUpdateMode = false,
+        onBackClick = {},
+        onDeleteNoteClick = {},
+        onSaveNoteClick = {},
+        onOpenColorPickerClick = {}
+    )
 }
