a0e89586ab2ae34ec09999f396055d37a6f7f88f
Behzod Halil <behzoddev@gmail.com>
2023-01-18 12:33:45 +0900

    Add SaveNoteHeaderUiComponent.kt

---
+++ 06-jetnotes-sample/src/main/java/uz/behzoddev/a06_jetnotes_sample/presentation/component/SaveNoteHeaderUiComponent.kt
@@ -0,0 +1,21 @@
+package uz.behzoddev.a06_jetnotes_sample.presentation.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SaveOrUpdateNoteHeaderComponent(
+    isUpdateMode: Boolean,
+    onBackClick: () -> Unit,
+    onSaveNoteClick: () -> Unit,
+    onOpenColorPickerClick: () -> Unit,
+    onDeleteNoteClick: () -> Unit
+) {
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SaveOrUpdateNoteHeaderComponentPreview() {
+
+}
