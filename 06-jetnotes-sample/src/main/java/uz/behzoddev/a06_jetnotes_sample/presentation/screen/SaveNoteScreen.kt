698052f40ddf3b3ed0ee40d44f67074d1d31ed9b
Behzod Halil <behzoddev@gmail.com>
2023-01-18 12:33:49 +0900

    Add SaveNoteScreen.kt

---
+++ 06-jetnotes-sample/src/main/java/uz/behzoddev/a06_jetnotes_sample/presentation/screen/SaveNoteScreen.kt
@@ -0,0 +1,19 @@
+package uz.behzoddev.a06_jetnotes_sample.presentation.screen
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SaveNoteScreen() {
+    Scaffold(
+        topBar = {},
+        content = {}
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SaveNoteScreenPreview() {
+
+}
