a0b04f88facedf18e6b15c4f481e7b622492c277
Behzod Halil <behzoddev@gmail.com>
2023-01-18 10:34:38 +0900

    Add NoteColorUi.kt

--- 06-jetnotes-sample/src/main/java/uz/behzoddev/a06_jetnotes_sample/screen/NoteColorUi.kt
+++ 06-jetnotes-sample/src/main/java/uz/behzoddev/a06_jetnotes_sample/screen/NoteColorUi.kt
@@ -7,11 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -19,7 +15,6 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun NoteColorUi(
