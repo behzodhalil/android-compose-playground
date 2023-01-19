bcebbc2a56441f9066c7ac364e88f1a2f547de23
Behzod Halil <behzoddev@gmail.com>
2023-01-19 15:10:12 +0900

    Add DrawerScreen.kt

---
+++ 07-reddit-sample/src/main/java/uz/behzoddev/a07_reddit_sample/ui/screens/DrawerScreen.kt
@@ -0,0 +1,15 @@
+package uz.behzoddev.a07_reddit_sample.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DrawerScreen() {
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DrawerScreenPreview() {
+
+}
